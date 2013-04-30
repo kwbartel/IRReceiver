2ec3a0888e7bf94dd6a5ceb725b4f6d04b82ee06
Katie Bartel <kwbartel@gmail.com>
2013-04-29 23:19:12 -0400

    lowered threshold

--- IRReceiver.ino
+++ IRReceiver.ino
@@ -2,13 +2,15 @@ int receiver = 5;
 int led = 6;
 int value = 0;
 int hit_value;
-int old_values[100];
+//int old_values[100];
 int count;
+int lives;
 
 void setup() {
-  hit_value = 1000;
-  fill_values();
+  hit_value = 950;
+ // fill_values();
   count = 0;
+  lives = 5;
   
   pinMode(led, OUTPUT);
   Serial.begin(9600);
@@ -16,13 +18,13 @@ void setup() {
 
 void loop() {
   value = analogRead(receiver);
-  old_values[count] = value;
+  /*old_values[count] = value;
   if (count < sizeof(old_values))
      count++;
    else
-     count = 0;
-  
-  Serial.println(average());
+     count = 0;                      
+  */
+  Serial.println(value); 
   if (value < hit_value)
     digitalWrite(led, HIGH);
   else
@@ -32,7 +34,7 @@ void loop() {
 /**
 * Initialize running list of values with values
 * higher than the threshold.
-*/
+*/ /*
 void fill_values()
 {
   for (int i = 0; i < sizeof(old_values); i++)
@@ -45,4 +47,4 @@ float average()
   for (int i = 0; i < sizeof(old_values); i++)
     avg += old_values[i];
   return avg / sizeof(old_values);
-}
+} */
